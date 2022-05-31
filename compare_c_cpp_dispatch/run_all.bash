01b20d88c7dc36bd728a29c3d1258c49ecb750f7
Trevor Kemp <trevor@trevorkemp.work>
2022-05-31 10:14:16 -0400

    Add new dispatch tests, variant and cptr double lookup. Add results.

--- compare_c_cpp_dispatch/run_all.bash
+++ compare_c_cpp_dispatch/run_all.bash
@@ -19,8 +19,10 @@ doit cfunct_if_noinline
 doit cfunct_switch
 doit cfunct_switch_noinline
 doit cptr
+doit cptr_double
 doit lambda
 doit pointer_lambda
 doit poly
+doit variant_poly
 doit bind_dynamic
 doit bind_static
